ff26681c15dcf027efc05cd8210071672f3e24e4
Varun Tanwar <newvaruntanwar@gmail.com>
2024-01-12 15:27:06 +0530

    Second version

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Git");
+
+        System.out.println("Saving this file to remote git");
     }
 }
